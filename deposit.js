4f45fb562f1a063e1a4b3ad0c1b88b70268bb47a
Gina Case <ginamcase@gmail.com>
2022-08-14 11:46:43 -0600

    withdraw and dep page working

--- deposit.js
+++ deposit.js
@@ -1,9 +1,9 @@
 function Deposit(){
   const ctx = React.useContext(UserContext); 
-  let inUser = ctx.loggedIn[0];
   const [update, setUpdate] = React.useState('false');
   const [value, setValue] = React.useState("");
   const [show, setShow] = React.useState(true);
+  let data = JSON.stringify(ctx.users[ctx.users.length-1].balance);
 
   const handleTextChange = (event) => {
     setValue(event.target.value);
@@ -12,7 +12,7 @@ function Deposit(){
   function handleDeposit(){
     let balance = document.getElementById("balance").value;
     if (balance > 0 && !isNaN(balance)) {
-    inUser.user.balance += Number(balance);
+    data += Number(balance);
     setUpdate(true);
     setShow(false);
     }
@@ -25,15 +25,13 @@ function Deposit(){
     <Card
     txtcolor="black"
     header="Deposit"
-    body= {show? (inUser ? ( 
+    body= {
       <>
-      <h5>{update ? "Balance: " + inUser.user.balance : "Balance: "+ inUser.user.balance}</h5>
+      <h5>{update ? "Balance: " + data : "Balance: "+ data}</h5>
       <h6>Deposit Amount:</h6>
       <input type="number" width="200" id="balance" onChange={handleTextChange} value={value}></input>
-      <button type="submit" disabled={ value ?false:true} className="btn btn-light" onClick={handleDeposit}>Submit</button>
-      </>
-
-      ) : ("Please Login")) : ( "Success! Balance is $" + inUser.user.balance)}
+      <button type="submit" disabled={ value ?false:true} className="btn btn-light" onClick={handleDeposit}>Deposit</button>
+      </>}
     />    
   );
 };
